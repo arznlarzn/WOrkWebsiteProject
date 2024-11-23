3aa11c7838cfad7b442ac409bfcdc0db1ecff92c
arznlarzn <larsonjdavis@gmail.com>
2024-11-23 04:36:22 -0800

    I added project page to the javascript logic, so this html project page is just going to be like the summary description. So when users click the link, it will take them to a new dedicated page where I will talk on why I made the project and the tools i used. I will include more details.

--- projectscript.js
+++ projectscript.js
@@ -6,31 +6,37 @@ document.addEventListener("DOMContentLoaded", function() {
             description: "<br>This is not a new idea. Building a website to use as a portfolio. However, not all teachers recommends it. I can understand why, having seen some websites that I think would be better not existing. You don't wanna fall in with that bunch.<br><br>But my decision to go through with this is pretty straight-forward:<br> - It sounded like a really fun project<br> - It would come with a ton of practical experience<br> - the skills gained are valuable right now<br> - Lastly, I'd rather try and fail and learn from it, than learn nothing at all.<br><br>See the github repository for details into the tools I used as well.",
             github: "index.html", // for now, but i will update this as soon as I upload these files to github.
             image: "/larson.jpg",
+            projectpage: "larsondavis.io",
         },
         "Web-Based Magic 8-Ball": {
             description: "<br>This is a web based Magic 8-Ball simulator- built on HTML, CSS, and JS. It's a very simple web app where most of it's visual elements have been made with CSS, and it's logic and responses are all built within the JavaScript file.<br><br>This was my first entry into using JavaScript. Really, really simple, but still, why not include it.",
             github: "will contain a specific url to the github repo",
             image: "/larson.jpg",
+            projectpage: "",
         },
         "A Desktop AI chatbot": {
             description: "This a personal ai chatbot built with python,a available right on your desktop! You can set it's personality too!",
             github: "will contain a specific url to the github repo",
             image: "/larson.jpg",
+            projectpage: "",
         },
         "An Unhinged Reddit Bot": {
             description: "This is an unhinged reddit bot lurking in the AITAH subreddit.",
             github: "will contain a specific url to the github repo",
             image: "/larson.jpg",
+            projectpage: "",
         },
         "Sobriety Calculator": {
             description: "This was like my very first python project where I actually built something. I built it using tkinter to create a GUI.",
             github: "will contain a specific url to the github repo",
             image: "/larson.jpg",
+            projectpage: "",
         },
         "A Custom Water Loop": {
             description: "This wasn't so much a coding project as it was a physical project, but it obviously takes knowing your basics of putting a windows computer together. But my custom-built hardline water-cooled machine is certainly not nothing. =]",
             github: "will contain a specific url to the github repo",
             image: "/larson.jpg",
+            projectpage: "",
         },
     };
 
@@ -47,6 +53,7 @@ function displayProject(projectKey) {
             <center><img src="${project.image}" alt="${projectKey}" class="img-fluid project-image"></center>
             <p>${project.description}</p>
             <a href="${project.github}" target="_blank">GitHub Repository</a>
+            <a href="${project.projectpage}" target="_blank">Project Page</a>
             `;
         }
     } 
@@ -69,3 +76,5 @@ function displayProject(projectKey) {
         });
     });
 });
+
+console.log("Say hi to your mom for me! =] -Larson");
